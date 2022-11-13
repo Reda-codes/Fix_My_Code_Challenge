eafe77f8ccbd396964a766725f091adda18d8f44
Reda <muhammadredabh@gmail.com>
2022-11-13 12:34:37 +0100

    fix

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -20,12 +20,13 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
+            i = 0
             break
         end
     end
     result << i_arg if !is_inserted
 end
 
-puts result.sort
+puts result
